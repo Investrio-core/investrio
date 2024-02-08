8027bc3e7ca9a278c67cc00cfcec7a594b4eb062
Pavel Trofimov <p.trofimov@gmail.com>
2024-02-08 15:06:56 +0300

    [INV] fix: Auth test

--- app/lib/hooks/useRefreshToken.ts
+++ app/lib/hooks/useRefreshToken.ts
@@ -3,20 +3,30 @@
 import { redirect } from "next/navigation";
 import axios from "../axios";
 import { signIn, useSession, signOut } from "next-auth/react";
+import { getCookies } from "cookies-next";
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+console.log(BASE_URL);
 
 export const useRefreshToken = () => {
   const { data: session, update } = useSession();
 
   const refreshToken = async () => {
     try { 
-      const res = await axios.post("/user/refresh", {}, { withCredentials: true });
+      const res = await fetch(`${BASE_URL}/user/refresh`, { credentials: 'include', method: "POST"});
 
-      if (!res.data.accessToken) {
-        await signOut()
+      const token = await res.json() as any
+
+
+      console.log(token);
+
+      if (!token.accessToken) {
+        // await signOut()
+        return 
       }
 
-      update({accessToken: res.data.accessToken})
-      if (session) session.user.accessToken = res.data.accessToken;
+      update({accessToken: token.accessToken.accessToken})
+      if (session) session.user.accessToken = token.accessToken.accessToken;
       else redirect('/auth/login');
     } catch (err) {
       await signOut()
