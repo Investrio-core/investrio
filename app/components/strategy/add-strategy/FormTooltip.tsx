07350561422932250a49334188c5abc22a69ea2b
Pavel Trofimov <p.trofimov@gmail.com>
2024-02-29 11:48:27 +0300

    [INV]: fix tooltip data

--- app/components/strategy/add-strategy/FormTooltip.tsx
+++ app/components/strategy/add-strategy/FormTooltip.tsx
@@ -11,37 +11,31 @@ export const StrategyFormTooltip = () => {
         <div className="flex flex-col gap-13 mt-3">
           <h3 className="font-bold">Check your statement</h3>
           <div>
-            You can find this information on your latest credit card statement
-            or your mobile app. Providing this information here allows Investrio
-            to calculate your debt-free plan. Your data is not shared or used
-            for any other purpose.
+          You can find this information on your latest credit card statement or your mobile app. Providing this information here allows Investrio to calculate your debt-free plan. Your data is not shared or used for any other purpose.
           </div>
         </div>
         <div className="flex flex-col gap-1 mt-3">
           <h3 className="font-bold">Name Your Debt</h3>
           <div>
-            Assign a descriptive name to each credit card or loan account for
-            easy tracking.
+          Assign a descriptive name to each credit card or loan account for easy tracking.
           </div>
+        </div>
+        <div className="flex flex-col gap-1 mt-3">
+          <h3 className="font-bold">Debt Type</h3>
+          <div>Currently we only support credit cards. Additional debt types coming soon!</div>
+        </div>
+        <div className="flex flex-col gap-1 mt-3">
+          <h3 className="font-bold">Interest Rate (APR)</h3>
+          <div>This is the amount that it costs you to borrow money. (Did you know: the average APR in the US is 24.99%!)</div>
         </div>
         <div className="flex flex-col gap-1 mt-3">
           <h3 className="font-bold">Outstanding Balance</h3>
           <div>This is the full amount that's owed on your card.</div>
         </div>
         <div className="flex flex-col gap-1 mt-3">
-          <h3 className="font-bold">Interest Rate (APR)</h3>
-          <div>This is the amount that it costs you to borrow money.</div>
-        </div>
-        <div className="flex flex-col gap-1 mt-3">
           <h3 className="font-bold">Minimum Payment</h3>
-          <div>Enter your most recent monthly minimum.</div>
-        </div>
-        <div className="flex flex-col gap-1 mt-3">
-          <h3 className="font-bold">Due Date</h3>
           <div>
-            This is when your monthly payment is due. (Did you know: You can ask
-            your credit card to change this date to match your paycheck if
-            needed).
+          Enter your most recent monthly minimum.
           </div>
         </div>
       </div>
