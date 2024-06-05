a2387b47e4f27d41ee0bd2e456bffa10a1093d37
Sean <sean2evo@hotmail.com>
2024-06-05 21:25:36 +0100

    [INV] removed middleware navigation redirection requiring subs

--- middleware.ts
+++ middleware.ts
@@ -4,51 +4,51 @@ import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
-  const params = request.nextUrl.searchParams
+  const params = request.nextUrl.searchParams;
 
   const token = await getToken({
     req: request,
-    secret: process.env.SECRET_KEY
-  })
+    secret: process.env.SECRET_KEY,
+  });
 
-  let cookieName = 'next-auth.session-token';
+  let cookieName = "next-auth.session-token";
 
-  if (process.env.NODE_ENV === 'production') {
-    cookieName = '__Secure-next-auth.session-token';
+  if (process.env.NODE_ENV === "production") {
+    cookieName = "__Secure-next-auth.session-token";
   }
-  
+
   const sessionToken = cookies().get(cookieName)?.value;
 
-  if ((!path.startsWith('/auth/login') && !path.startsWith('/auth/signup')) && !sessionToken) {
-    return NextResponse.redirect(new URL('/auth/login', request.url));
+  if (
+    !path.startsWith("/auth/login") &&
+    !path.startsWith("/auth/signup") &&
+    !sessionToken
+  ) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
   }
 
-  if (path.startsWith('/settings') && params.has('success') && sessionToken) {
+  if (path.startsWith("/settings") && params.has("success") && sessionToken) {
     return NextResponse.next();
   }
 
-  if ((token?.isShowPaywall) && sessionToken && !path.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
-  }
+  // if ((token?.isShowPaywall) && sessionToken && !path.startsWith('/dashboard')) {
+  //   return NextResponse.redirect(new URL('/dashboard', request.url));
+  // }
 
-  if (!path.includes('/dashboard') && !token?.isAddedFreeStrategy && sessionToken) {
-    return NextResponse.redirect(new URL('/dashboard/debts/add', request.url));
-  }
-  
-  if ((path.startsWith('/auth/login') || path.startsWith('/auth/signup')) && sessionToken) {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
-  }
-  else {
+  // if (!path.includes('/dashboard') && !token?.isAddedFreeStrategy && sessionToken) {
+  //   return NextResponse.redirect(new URL('/dashboard/debts/add', request.url));
+  // }
+
+  if (
+    (path.startsWith("/auth/login") || path.startsWith("/auth/signup")) &&
+    sessionToken
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  } else {
     return NextResponse.next();
   }
 }
 
 export const config = {
-  matcher: [
-    "/",
-    "/auth/:path*",
-    "/dashboard/:path*",
-    "/budget",
-    '/settings'
-  ],
+  matcher: ["/", "/auth/:path*", "/dashboard/:path*", "/budget", "/settings"],
 };
