db92b4fb1a6bf9046e5eb9484fcd308d3088fa88
Sean <sean2evo@hotmail.com>
2024-07-06 05:28:30 +0100

    adjusted google button to match figma

--- app/components/ui/buttons/GoogleSignInButton.tsx
+++ app/components/ui/buttons/GoogleSignInButton.tsx
@@ -4,29 +4,47 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Mixpanel from "@/services/mixpanel";
 
 const GoogleSignInButton = () => {
-    const { data: session } = useSession();
+  const { data: session } = useSession();
 
-    const auth = async () => {
-      await signIn('google', {callbackUrl: `${process.env.NEXTAUTH_URL}/dashboard?success=true`},)
-    }
-    if (session && session.user) {
-        return (
-            <div className="flex gap-4 ml-auto">
-                <p className="text-sky-600">{session.user.name}</p>
-                <button onClick={() => signOut()} className="text-red-600">
-                    Sign Out
-                </button>
-            </div>
-        );
-    }
+  const auth = async () => {
+    await signIn("google", {
+      callbackUrl: `${process.env.NEXTAUTH_URL}/dashboard?success=true`,
+    });
+  };
+  if (session && session.user) {
+    // text-sm md:text-base
     return (
-        <button onClick={auth} className="btn btn-ghost w-full bg-[#F2F5F7] border-[#EAEAEA]">
-            <span className="flex items-center">
-                <img loading="lazy" height="24" width="24" id="provider-logo-dark" src="https://authjs.dev/img/providers/google.svg" className="w-5 h-5 mr-5" />
-                Access with Google
-            </span>
+      <div className="flex gap-4 ml-auto">
+        <p className="text-sky-600">{session.user.name}</p>
+        <button onClick={() => signOut()} className="text-red-600">
+          Sign Out
         </button>
+      </div>
     );
+  }
+  return (
+    <button
+      onClick={auth}
+      className="btn btn-ghost w-full bg-[#F2F5F7] border-[#EAEAEA]"
+      style={{
+        borderRadius: "8px",
+        background: "#FFF",
+        boxShadow: "0px 4px 10px 0px rgba(0, 0, 0, 0.08)",
+      }}
+    >
+      <span className="flex items-center capitalize text-slate-950 text-base font-medium">
+        <img
+          loading="lazy"
+          height="24"
+          width="24"
+          id="provider-logo-dark"
+          src="https://authjs.dev/img/providers/google.svg"
+          className="w-5 h-5 mr-5"
+        />
+        Login with Google
+      </span>
+    </button>
+  );
 };
 
 export default GoogleSignInButton;
