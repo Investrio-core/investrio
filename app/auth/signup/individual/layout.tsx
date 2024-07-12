cc222cab8951b370641b9ba5080e1655880d99f8
Sean <sean2evo@hotmail.com>
2024-07-12 10:40:26 +0100

    deleted unnecessary extra page

--- app/auth/signup/individual/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function IndividualLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="max-w-[600px] mx-auto">
-      {children}
-    </div>
-  )
-}
