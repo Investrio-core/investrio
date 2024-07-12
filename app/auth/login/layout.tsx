5549e8cc1e0b5c29668b33d2af71b9c661ff9257
Sean <sean2evo@hotmail.com>
2024-07-12 10:39:08 +0100

    adjust layout of login

--- app/auth/login/layout.tsx
+++ app/auth/login/layout.tsx
@@ -5,7 +5,7 @@ export default function AuthLayout({
 }) {
   return (
     <>
-      <div className="flex h-screen min-h-screen justify-center overflow-auto bg-violet-50">
+      <div className="flex h-screen min-h-screen justify-center items-center overflow-auto bg-violet-50">
         <div className="w-full max-w-[90%] md:max-w-lg">
           <div>{children}</div>
         </div>
