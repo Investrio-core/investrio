b2100fbc6836fa952b796597ad53c9c0f84f4ce7
Sean <sean2evo@hotmail.com>
2024-07-12 11:08:32 +0100

    adjusted icon button

--- app/components/ui/IconButton/index.tsx
+++ app/components/ui/IconButton/index.tsx
@@ -1,25 +1,25 @@
 import React from "react";
-import { twMerge } from 'tailwind-merge'
-import './styles.css'
+import { twMerge } from "tailwind-merge";
+import "./styles.css";
 
 type CardProps = {
-    className?: string,
-    hoverClassName?: string,
-    onClick?: () => void,
-    Icon: React.ComponentType
-  };
+  className?: string;
+  hoverClassName?: string;
+  onClick?: () => void;
+  Icon: React.ComponentType;
+  disabled?: boolean;
+};
 
-const IconButton = ({
-    className,
-    onClick,
-    Icon
-}: CardProps) => {
+const IconButton = ({ className, onClick, Icon, disabled }: CardProps) => {
+  return (
+    <button
+      className={twMerge(className, "iconButton")}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      <Icon />
+    </button>
+  );
+};
 
-    return (
-        <button className={twMerge(className, 'iconButton')} onClick={onClick} >
-            <Icon />
-        </button>
-    )
-}
-
-export default IconButton
+export default IconButton;
