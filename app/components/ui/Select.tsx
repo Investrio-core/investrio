59a80c39ce854b076f8867ffcd19ca3ae20a1a05
Sean <sean2evo@hotmail.com>
2024-07-12 11:12:56 +0100

    added optional placeholder to select component

--- app/components/ui/Select.tsx
+++ app/components/ui/Select.tsx
@@ -5,10 +5,10 @@ import { ChangeEventHandler } from "react";
 type SelectProps = {
   label: string;
   name: string;
-  value?: string;
+  value?: string | undefined;
   options: {
     label: string;
-    value: string;
+    value: string | undefined;
   }[];
   required?: boolean;
   inline?: boolean;
@@ -16,19 +16,19 @@ type SelectProps = {
   className?: string;
   onChange?: ChangeEventHandler<HTMLSelectElement>;
   disabled?: boolean;
+  placeholder?: string;
 };
-export default function Select(
-  {
-    label,
-    name,
-    options,
-    defaultValue,
-    inline,
-    value,
-    disabled,
-    onChange,
-  }: SelectProps
-) {
+export default function Select({
+  label,
+  name,
+  options,
+  defaultValue,
+  inline,
+  value,
+  disabled,
+  onChange,
+  placeholder,
+}: SelectProps) {
   return (
     <div className={twMerge(`form-control`, inline ? "flex flex-col" : "")}>
       <label className={"text-left font-light"}>
@@ -38,12 +38,22 @@ export default function Select(
         disabled={disabled}
         value={value}
         className={clsx(
-          "select select-bordered w-[100%] h-10 border-[#EDF2F6] bg-[#F8F8F8] text-[#747682] text-base",
+          "select select-bordered w-[100%] h-10 border-[#EDF2F6] bg-[#F8F8F8] text-[#747682] text-base"
         )}
         name={name}
         defaultValue={defaultValue}
         onChange={onChange}
       >
+        {placeholder ? (
+          <option
+            className={"text-slate-500"}
+            value={undefined}
+            selected
+            disabled
+          >
+            {placeholder}
+          </option>
+        ) : null}
         {options.map((option) => (
           <option
             className="text-[#747682] text-base w-full p-3"
