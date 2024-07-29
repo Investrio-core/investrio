41e37adbf9c7ccef6d9b8b2359a4a21310008595
Sean <sean2evo@hotmail.com>
2024-07-29 09:46:01 -0500

    percents now stored as number not percentage

--- lib/formatDebtsForApi/index.ts
+++ lib/formatDebtsForApi/index.ts
@@ -1,28 +1,33 @@
 import { DebtFormType, FinancialRecord } from "@/types/debtFormType";
 
-
-export const formatDebtsForApi = ( userId: string ,debts: DebtFormType[], extraPayAmount: string ) => {
+export const formatDebtsForApi = (
+  userId: string,
+  debts: DebtFormType[],
+  // debt?: DebtFormType,
+  extraPayAmount?: string
+) => {
   let result = debts.map((debt) => {
-
     const data: FinancialRecord = {
-    userId,
-    debtTitle: debt.debtName,
-    minPayAmount: parseFloat(debt.minPayment.replace("$", "").replaceAll(",", "")),
-    interestRate: parseFloat(debt.rate.replace("%", "")) / 100,
-    debtType: debt.debtType,
-    initialBalance: parseFloat(
-      debt.balance.replace("$", "").replaceAll(",", "")
-    ),
-    extraPayAmount: parseFloat(extraPayAmount),
-    periodicity: "MONTH",
-  }
+      userId,
+      debtTitle: debt.debtName,
+      minPayAmount: parseFloat(
+        debt.minPayment.replace("$", "").replaceAll(",", "")
+      ),
+      interestRate: parseFloat(debt.rate.replace("%", "")), /// 100,
+      debtType: debt.debtType,
+      initialBalance: parseFloat(
+        debt.balance.replace("$", "").replaceAll(",", "")
+      ),
+      extraPayAmount: extraPayAmount ? parseFloat(extraPayAmount) : 0,
+      periodicity: "MONTH",
+    };
 
-  if (debt?.id) {
-    data['id'] = debt.id
-  }
+    if (debt?.id) {
+      data["id"] = debt.id;
+    }
 
-  return data
-}); 
+    return data;
+  });
 
   return result;
 };
