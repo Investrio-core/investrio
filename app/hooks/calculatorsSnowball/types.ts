dd9071f015d12e37032e68f3e3c216939e4c4de5
Sean <sean2evo@hotmail.com>
2024-07-29 09:43:49 -0500

    types for Snowball/Avalanche calculator

---
+++ app/hooks/calculatorsSnowball/types.ts
@@ -0,0 +1,67 @@
+export interface AccountObject {
+  name: string;
+  balance: number;
+  interest: number;
+  minPayment: number;
+}
+
+export enum AccountObjectKeys {
+  name = "name",
+  balance = "balance",
+  interest = "interest",
+  minPayment = "minPayment",
+}
+
+export enum OrderDirection {
+  ascending = "ascending",
+  descending = "descending",
+}
+
+export enum RepaymentStrategy {
+  avalanche = "avalanche",
+  snowball = "snowball",
+}
+
+export type Payment = {
+  balance: number;
+  accounts: AccountObject[];
+};
+
+export interface PaymentOptions {
+  balance: number;
+  interest: number;
+  payment: number;
+}
+
+export interface PaymentObject {
+  balanceStart: number;
+  balanceEnd: number;
+  accruedInterest: number;
+  additionalPayment: number;
+  paymentAmount: number;
+  minPayment: number;
+  name: string;
+  paymentDate: Date | string;
+}
+
+export interface PaymentPlanObject {
+  balance: number;
+  accounts: PaymentObject[];
+}
+
+export interface ResultsObject {
+  totalInterestPaid: number;
+  totalPayments: number;
+  payments: Array<PaymentPlanObject>;
+}
+
+export interface AccountMonth {
+  balanceStart: number;
+  balanceEnd: number;
+  accruedInterest: number;
+  additionalPayment: number;
+  paymentAmount: number;
+  minPayment: number;
+  name: string;
+  paymentDate: Date | string;
+}
