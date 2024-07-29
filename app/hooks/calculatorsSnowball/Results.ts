8170e2199b462641624183e828618d96c12c35c3
Sean <sean2evo@hotmail.com>
2024-07-29 09:42:15 -0500

    Results class for Snowball/Avalanche calculator

---
+++ app/hooks/calculatorsSnowball/Results.ts
@@ -0,0 +1,28 @@
+import { toCurrency } from "./helpers";
+import type { PaymentPlanObject, ResultsObject } from "./types";
+
+class Results implements ResultsObject {
+  totalInterestPaid = 0;
+  totalPayments = 0;
+  strategy = "";
+  payments: PaymentPlanObject[] = [];
+
+  constructor(data: PaymentPlanObject[], strategy: string) {
+    this.payments = [...data];
+    this.totalInterestPaid = this.calculateTotalInterestPaid();
+    this.totalPayments = data.length;
+    this.strategy = strategy;
+  }
+
+  calculateTotalInterestPaid() {
+    const value = this.payments.reduce((acc: number, item) => {
+      const monthlyInterest = item.accounts.reduce((output, account) => {
+        return output + account.accruedInterest;
+      }, 0);
+      return acc + monthlyInterest;
+    }, 0);
+    return toCurrency(value);
+  }
+}
+
+export default Results;
