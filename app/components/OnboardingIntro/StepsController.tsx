922ff6c2725e258f02a72aaeab0d9aa0786ed28d
Sean <sean2evo@hotmail.com>
2024-08-18 21:01:22 -0500

    onboarding steps added functionality for embedded content and other things

--- app/components/OnboardingIntro/StepsController.tsx
+++ app/components/OnboardingIntro/StepsController.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { StandardButton } from "../ui/buttons";
+import React from "react";
 
 interface Props {
   setNext: Function;
@@ -10,6 +11,94 @@ interface Props {
   useButton?: boolean;
   renderLastNext?: boolean;
   classes?: string;
+  content?: JSX.Element;
+  stepsIterator?: number[];
+  renderDirection?: "horizontal" | "vertical";
+}
+
+function StepsControllerVertical({
+  setNext,
+  setPrev,
+  setSkip,
+  currentStep,
+  numSteps,
+  useButton = false,
+  renderLastNext = false,
+  classes = "",
+  content,
+  stepsIterator,
+}: Props) {
+  const renderNext = currentStep < numSteps - 1;
+  return (
+    <div
+      className={classes}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100vw",
+        justifyContent: "space-between",
+        alignItems: "center",
+        alignSelf: "center",
+        justifySelf: "center",
+      }}
+    >
+      <div className="justify-center align-center items-start gap-[4px] flex">
+        {stepsIterator &&
+          stepsIterator.map((discard, idx) => (
+            <Image
+              src={`${
+                idx === currentStep
+                  ? "/images/step-selected-green.svg"
+                  : "/images/step-unselected-green.svg"
+              }`}
+              key={`step-${idx}`}
+              alt={"step"}
+              width={16}
+              height={16}
+            />
+          ))}
+      </div>
+      <div>{content}</div>
+      <div className="w-[100%] flex justify-center align-center">
+        {currentStep < numSteps - 1 || renderLastNext ? (
+          <div
+            // className={`btn btn-primary text-right text-zinc-800 text-lg font-medium leading-7 cursor-pointer w-[100%] mt-[45px]`}
+            className={
+              "mt-[30px] w-[100%] mx-[8px] h-12 py-2 bg-[#8833ff] mx-[12px] rounded-lg justify-center items-center gap-2.5 inline-flex"
+            }
+            style={{
+              borderRadius: "12px",
+              position: "relative",
+              top: "-10px",
+            }}
+            onClick={() => setNext()}
+          >
+            <div className="text-center text-white text-base font-semibold leading-normal">
+              Next
+            </div>
+          </div>
+        ) : null}
+        {currentStep !== 0 && !renderNext ? (
+          <div
+            // className="btn btn-primary text-left text-zinc-800 text-lg font-medium leading-7 cursor-pointer w-[100%] mt-[45px]"
+            className={
+              "mt-[30px] w-[100%] mx-[8px] h-12 py-2 bg-[#8833ff] mx-[12px] rounded-lg justify-center items-center gap-2.5 inline-flex"
+            }
+            style={{
+              borderRadius: "12px",
+              position: "relative",
+              top: "-10px",
+            }}
+            onClick={() => setPrev()}
+          >
+            <div className="text-center text-white text-base font-semibold leading-normal">
+              Previous
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
 }
 
 export default function StepsController({
@@ -21,8 +110,26 @@ export default function StepsController({
   useButton = false,
   renderLastNext = false,
   classes = "",
+  renderDirection = "horizontal",
+  content,
 }: Props) {
   const stepsIterator = Array(numSteps).fill(0);
+
+  if ((renderDirection = "vertical")) {
+    return (
+      <StepsControllerVertical
+        setNext={setNext}
+        setPrev={setPrev}
+        setSkip={setSkip}
+        currentStep={currentStep}
+        numSteps={numSteps}
+        renderLastNext={renderLastNext}
+        stepsIterator={stepsIterator}
+        content={content}
+        classes={classes}
+      />
+    );
+  }
 
   const nextButton = useButton ? (
     <button
@@ -77,8 +184,8 @@ export default function StepsController({
           <Image
             src={`${
               idx === currentStep
-                ? "/images/step-selected.svg"
-                : "/images/step-unselected.svg"
+                ? "/images/step-selected-green.svg"
+                : "/images/step-unselected-green.svg"
             }`}
             key={`step-${idx}`}
             alt={"step"}
