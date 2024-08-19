3e9095f055888ad86ff51a540c5308a8d4c8f185
Sean <sean2evo@hotmail.com>
2024-08-18 23:27:03 -0500

    style cleanup

--- app/components/dashboard/Card.tsx
+++ app/components/dashboard/Card.tsx
@@ -47,9 +47,9 @@ export const Card = ({
   };
 
   return (
-    <div className="relative max-w-fit-content lg:max-w-fit-content flex justify-between gap-0 lg:gap-4 rounded-xl bg-white w-full ">
-      <div className="py-0 my-0 rounded-[18px] border border-[#b1b2ff]/80 py-[3px] px-[9px]">
-        <div className="flex flex-col lg:flex-row items-center min-w-fit-content py-0 my-0 lg:mb-4">
+    <div className="relative max-w-fit-content lg:max-w-fit-content max-h-fit-content flex justify-between items-center gap-0 lg:gap-4 rounded-xl bg-white w-full ">
+      <div className="py-0 my-0 rounded-[18px] border border-[#b1b2ff]/80 py-[4px] px-[12px]">
+        <div className="flex flex-col lg:flex-row justify-center items-center min-w-fit-content py-0 my-0 lg:mb-4">
           <div>{renderCorrectIcon()}</div>
           <span className="text-gray-500 font-normal text-xs lg:text-normal text-base leading-6 ml-2 whitespace-nowrap lg:whitespace-normal text-center">
             {label}
