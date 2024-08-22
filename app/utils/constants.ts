0338c37b295d5d13b13a8df4a855c596ccbca422
Sean <sean2evo@hotmail.com>
2024-08-22 14:53:36 -0500

    made all core routes constants for easy changability

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,1 +1,4 @@
-export const backgroundColor = "bg-white" // bg-violet-50
+export const backgroundColor = "bg-white"; // bg-violet-50
+export const DEBTS_PAGE = "/dashboard/debts";
+export const BUDGET_PAGE = "/dashboard/budget";
+export const HOME_DASHBOARD_PAGE = "/dashboard";
