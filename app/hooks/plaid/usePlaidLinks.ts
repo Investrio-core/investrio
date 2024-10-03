562dbcc3061ff5b5c95b75fe0f81d37a30145722
Sean <sean2evo@hotmail.com>
2024-10-03 17:03:03 -0500

    added storing of links

---
+++ app/hooks/plaid/usePlaidLinks.ts
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosAuth from "@/app/hooks/useAxiosAuth";
+import { useMemo } from "react";
+
+const categories = ["needs", "wants", "savings", "debts"];
+
+export default function usePlaidLinks(date?: Date | null) {
+  const axiosAuth = useAxiosAuth();
+  const year = date?.getFullYear() ?? new Date().getFullYear();
+  const month = date?.getMonth() ?? new Date().getMonth();
+
+  const { data, isLoading, isFetched, refetch } = useQuery({
+    queryKey: ["plaidLinks"],
+    queryFn: async () => {
+      const plaidLinks = await axiosAuth.get(`/plaid/getPlaidLinks`);
+      console.log("got plaid links");
+      console.log(plaidLinks);
+      return plaidLinks;
+    },
+    // refetchOnMount: true,
+    // refetchOnWindowFocus: true,
+    // enabled: !!(year && month), // !!date,
+  });
+
+  return {
+    plaidLinks: data,
+  };
+}
