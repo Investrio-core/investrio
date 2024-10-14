d7f0d6924082d623cd678d34b4f1ca91167c287e
Sean <sean2evo@hotmail.com>
2024-10-13 23:37:06 -0500

    adding bot context to dashboard components

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import React from "react";
 import { backgroundColor } from "@/app/utils/constants";
+import ChatWindow from "@/app/components/Chatbot/ChatWindow";
+import { BotProvider } from "@/app/context/BotContext/context";
 
 export default function DebtsLayout({
   children,
@@ -9,10 +13,13 @@ export default function DebtsLayout({
   return (
     //  h-screen
     // <div className="flex min-h-[91vh] justify-center items-center overflow-auto bg-violet-50"></div>
-    <div className={`flex min-h-[91vh] overflow-auto ${backgroundColor}`}>
-      <div className="w-full flex">
-        <div className="w-full flex">{children}</div>
+    <BotProvider>
+      <div className={`flex min-h-[91vh] overflow-auto ${backgroundColor}`}>
+        <div className="w-full flex">
+          <div className="w-full flex">{children}</div>
+        </div>
       </div>
-    </div>
+      <ChatWindow />
+    </BotProvider>
   );
 }
