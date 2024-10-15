24104eb82963a529e52b8e3d728fa3391625d1f8
Sean <sean2evo@hotmail.com>
2024-10-15 03:52:24 -0500

    update account function for use with swiping/classifying accounts

--- app/hooks/plaid/usePlaidLinks.ts
+++ app/hooks/plaid/usePlaidLinks.ts
@@ -1,13 +1,35 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import useAxiosAuth from "@/app/hooks/useAxiosAuth";
 import { useMemo } from "react";
 
 const categories = ["needs", "wants", "savings", "debts"];
 
+export enum AccountCategory {
+  PERSONAL = 0,
+  BUSINESS,
+  MIXED,
+}
+
 export default function usePlaidLinks(date?: Date | null) {
   const axiosAuth = useAxiosAuth();
+  const queryClient = useQueryClient();
   const year = date?.getFullYear() ?? new Date().getFullYear();
   const month = date?.getMonth() ?? new Date().getMonth();
+
+  // Partial<PlaidAccount> & { id: string; itemId: string };
+  const updateAccount = async (updatedAccount: {
+    id: string;
+    itemId: string;
+    accountCategory: "PERSONAL" | "BUSINESS" | "MIXED";
+  }) => {
+    const updatedAccountResult = await axiosAuth.post("/plaid/updateAccount", {
+      updatedAccount: { ...updatedAccount },
+    });
+    console.log("got account update result");
+    console.log(updatedAccountResult);
+    queryClient.invalidateQueries({ queryKey: ["plaidLinks"] });
+    return updatedAccountResult;
+  };
 
   const { data, isLoading, isFetched, refetch } = useQuery({
     queryKey: ["plaidLinks"],
@@ -20,11 +42,14 @@ export default function usePlaidLinks(date?: Date | null) {
     staleTime: 45 * (60 * 1000), // 45 mins
     refetchOnWindowFocus: false, // default: true
     refetchOnMount: false,
+
     // refetchOnWindowFocus: true,
     // enabled: !!(year && month), // !!date,
   });
 
   return {
     plaidLinks: data,
+    refetch,
+    updateAccount,
   };
 }
