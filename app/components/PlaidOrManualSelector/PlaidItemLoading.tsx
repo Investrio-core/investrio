5fbff9c0559862dce112c34df77806c4fc5cd70b
Sean <sean2evo@hotmail.com>
2024-10-16 01:14:26 -0500

    added logo to loader, added input values

--- app/components/PlaidOrManualSelector/PlaidItemLoading.tsx
+++ app/components/PlaidOrManualSelector/PlaidItemLoading.tsx
@@ -2,15 +2,37 @@
 
 import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import SvgLogo from "@/public/logo-background-svg.svg";
+import PngLogo from "@/public/logo-background-png.png";
+import Image from "next/image";
+import { ValidLoadSteps } from "@/app/hooks/plaid/usePlaidItem";
+
+const logo = <Image src={PngLogo} width={12} height={12} alt="Logo" />;
+const svgLogo = (
+  <SvgLogo style={{ width: "12px", height: "12px", color: "white" }} />
+);
 
 const loadingStates = [
+  "Creating Link",
   "Loading Accounts",
   "Loading Debts",
   "Loading Transactions",
   "All bank information loaded",
 ];
 
-export default function PlaidItemLoading() {
+interface Props {
+  linkCreating: boolean;
+  linkSuccessful: boolean;
+  loadStepInProgress: ValidLoadSteps;
+  loadingData: boolean;
+}
+
+export default function PlaidItemLoading({
+  linkCreating,
+  linkSuccessful,
+  loadStepInProgress,
+  loadingData,
+}: Props) {
   const [currentState, setCurrentState] = useState(0);
 
   useEffect(() => {
@@ -20,11 +42,47 @@ export default function PlaidItemLoading() {
 
     return () => clearTimeout(timer);
   }, [currentState]);
+
+  useEffect(() => {
+    if (linkCreating) {
+      setCurrentState(0);
+    } else if (linkSuccessful && loadingData) {
+      if (loadStepInProgress === "accounts") {
+        setCurrentState(1);
+      } else if (loadStepInProgress === "debts") {
+        setCurrentState(2);
+      } else if (loadStepInProgress === "transactions") {
+        setCurrentState(3);
+      }
+    }
+  }, [linkCreating, linkSuccessful, loadStepInProgress, loadingData]);
+
   //  background: "linear-gradient(135deg, #00d1c4 0%,   #8833ff 100%)",
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-[#8833ff] to-[#00d1c4] rounded-[50px]">
+      {/* <Image
+        src={PngLogo}
+        width={120}
+        height={120}
+        style={{ position: "absolute", bottom: 0, right: 250 }}
+        alt="Logo"
+      /> */}
+
       <div className="relative w-64 h-64">
         {/* Background pulse animation */}
+        <SvgLogo
+          style={{
+            width: "260px",
+            height: "260px",
+            position: "absolute",
+            top: 80,
+            left: 50,
+            zIndex: 500,
+            // top: 800,
+            // right: 350,
+          }}
+          //   className="top-18 left-12"
+        />
         <motion.div
           className="absolute inset-0 bg-blue-500 rounded-full opacity-20"
           animate={{
@@ -60,6 +118,7 @@ export default function PlaidItemLoading() {
               transition={{ duration: 0.5 }}
               className="text-white text-lg font-bold text-center"
             >
+              <div className="z-500">{logo}</div>
               {loadingStates[currentState]}
             </motion.div>
           </AnimatePresence>
@@ -73,7 +132,7 @@ export default function PlaidItemLoading() {
 }
 
 function FinancialSymbols() {
-  const symbols = ["$", "€", "£", "¥", "%", "📊", "💹"];
+  const symbols = ["$", "€", "£", "¥", "%"]; //, "📊", "💹"];
   return (
     <>
       {symbols.map((symbol, index) => (
