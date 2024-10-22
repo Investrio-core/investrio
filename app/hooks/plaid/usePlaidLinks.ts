2b324564e647e348e68854c83f6199b2114e879a
Sean <sean2evo@hotmail.com>
2024-10-22 05:22:36 -0500

    major new functionality for managing state changes

--- app/hooks/plaid/usePlaidLinks.ts
+++ app/hooks/plaid/usePlaidLinks.ts
@@ -1,6 +1,7 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import useAxiosAuth from "@/app/hooks/useAxiosAuth";
 import { useMemo } from "react";
+import { MdDataArray } from "react-icons/md";
 
 const categories = ["needs", "wants", "savings", "debts"];
 
@@ -13,36 +14,121 @@ export enum AccountCategory {
 export default function usePlaidLinks(date?: Date | null) {
   const axiosAuth = useAxiosAuth();
   const queryClient = useQueryClient();
-  const year = date?.getFullYear() ?? new Date().getFullYear();
-  const month = date?.getMonth() ?? new Date().getMonth();
+  // const year = date?.getFullYear() ?? new Date().getFullYear();
+  // const month = date?.getMonth() ?? new Date().getMonth();
+
+  const resetItemAccountsCategories = async (itemId: string) => {
+    console.log("resetting accounts");
+    console.log(itemId);
+
+    const updatedAccountsResult = await axiosAuth.post(
+      "/plaid/updateAccounts",
+      {
+        itemId,
+        updatedAccount: { accountCategory: null },
+      }
+    );
+    console.log("updated accounts result");
+    console.log(updatedAccountsResult);
+    queryClient.invalidateQueries({ queryKey: ["plaidLinks"] });
+    return updatedAccountsResult;
+  };
+
+  const resetAccountCategory = async (id: string, itemId: string) => {
+    console.log("-- resetting account --");
+    console.log(id);
+    console.log(itemId);
+    const updatedAccountResult = await axiosAuth.post("/plaid/updateAccount", {
+      updatedAccount: { id, itemId, accountCategory: null },
+    });
+    queryClient.invalidateQueries({ queryKey: ["plaidLinks"] });
+    return updatedAccountResult;
+  };
 
   // Partial<PlaidAccount> & { id: string; itemId: string };
   const updateAccount = async (updatedAccount: {
     id: string;
     itemId: string;
-    accountCategory: "PERSONAL" | "BUSINESS" | "MIXED";
+    accountCategory: "Personal" | "Business" | "Mixed";
   }) => {
     const updatedAccountResult = await axiosAuth.post("/plaid/updateAccount", {
       updatedAccount: { ...updatedAccount },
     });
-    console.log("got account update result");
-    console.log(updatedAccountResult);
     queryClient.invalidateQueries({ queryKey: ["plaidLinks"] });
     return updatedAccountResult;
+  };
+
+  const isClassifiedPredicate = (acc: any) =>
+    acc.accountCategory === "Personal" ||
+    acc.accountCategory === "Business" ||
+    acc.accountCategory === "Mixed";
+
+  const getUnclassifiedAccounts = () => {
+    console.log("accounts");
+    console.log(data);
+    console.log(data?.data?.success);
+    console.log(
+      data?.data?.success
+        // ?.map((dataArr) => dataArr.accounts)
+        ?.flatMap((dataArr) => dataArr.accounts)
+        ?.filter((acc) => !isClassifiedPredicate(acc))
+    );
+
+    return (
+      data?.data?.success
+        // ?.map((dataArr) => dataArr.accounts))
+        ?.flatMap((dataArr) => dataArr.accounts)
+        ?.filter((acc) => !isClassifiedPredicate(acc))
+    );
+  };
+
+  const allAccountsClassifiedNoId = () => {
+    const allAccounts = data?.data?.success
+      ?.map((dataArr) => dataArr.accounts)
+      .flatMap();
+    console.log(allAccounts);
+
+    const allAccountsClassified = allAccounts?.every(isClassifiedPredicate);
+
+    console.log("Classifying all accounts");
+    console.log(allAccounts);
+    console.log(allAccountsClassified);
+
+    return allAccountsClassified;
+  };
+
+  const allAccountsClassified = (itemId?: string) => {
+    if (!itemId) {
+      return allAccountsClassifiedNoId();
+    }
+
+    if (itemId) {
+      console.log(
+        "are all accounts with this itemId classified, itemId: ",
+        itemId
+      );
+      const currentItemAccounts = data?.data?.success?.accounts?.filter(
+        (acc: any) => acc.itemId === itemId
+      );
+      console.log(currentItemAccounts);
+      const allAccountsClassified = currentItemAccounts?.every(
+        isClassifiedPredicate
+      );
+      console.log(allAccountsClassified);
+
+      return allAccountsClassified;
+    }
   };
 
   const { data, isLoading, isFetched, refetch } = useQuery({
     queryKey: ["plaidLinks"],
     queryFn: async () => {
       const plaidLinks = await axiosAuth.get(`/plaid/getPlaidLinks`);
-      console.log("got plaid links");
-      console.log(plaidLinks);
       return plaidLinks;
     },
     staleTime: 45 * (60 * 1000), // 45 mins
     refetchOnWindowFocus: false, // default: true
     refetchOnMount: false,
-
     // refetchOnWindowFocus: true,
     // enabled: !!(year && month), // !!date,
   });
@@ -51,5 +137,9 @@ export default function usePlaidLinks(date?: Date | null) {
     plaidLinks: data,
     refetch,
     updateAccount,
+    allAccountsClassified,
+    getUnclassifiedAccounts,
+    resetAccountCategory,
+    resetItemAccountsCategories,
   };
 }
