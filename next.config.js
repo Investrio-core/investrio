8b34868de628fec091d0c093bc0f6f9d25b313da
Sean <sean2evo@hotmail.com>
2024-10-23 02:44:18 -0500

    formatting

--- next.config.js
+++ next.config.js
@@ -1,29 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true
+    ignoreBuildErrors: true,
   },
   images: {
     remotePatterns: [
       {
-        hostname: 'lh3.googleusercontent.com',
-      }
-    ]
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
   },
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
-    MIXPANEL_TOKEN: process.env.MIXPANEL_TOKEN
+    MIXPANEL_TOKEN: process.env.MIXPANEL_TOKEN,
   },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    })
+      use: ["@svgr/webpack"],
+    });
 
-    return config
+    return config;
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
